5fa9c0e25707a3644af76722305fdf9540e0826b
Anthony la'O <anthonylao@live.com>
2018-09-01 15:56:37 -0700

    Set up heroku, keys, gemfile, and routes

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
+    render html: "hello, world"
     # render html: “¡Hola, mundo!”
-    render html: "goodbye, world!"
+    # render html: "goodbye, world!"
   end
 end
